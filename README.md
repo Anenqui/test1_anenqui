622676e364bfd7e4bc106cd9cddf8958fe5d6f08
Anenqui <anenquipintadovenegas@gmail.com>
2026-02-12 19:30:10 -0600

    cambios en el index y readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # test1_anenqui
 un repositorio para mostrar mis skills en git
+
+ ## institucion escolar
+UTSelva
+
+ ## lugar de providencia
+ Yajalñon, chiapas
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anenqui´s Page</title>
+</head>
+<body>
+    <h1>Hola a la pag de Anenqui </h1>
+</body>
+</html>
